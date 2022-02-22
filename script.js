6237267f40e3aa6d12d58d755b6091ccff5de15a
Aazam Thakur <aazamthakur@gmail.com>
2022-02-23 00:11:15 +0530

    Added colors, black, need to add greyscale

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-const container = document.querySelector('.container')
-grid(16)
-
+const container = document.querySelector('.container');
 
 function randomBgColor(){
     let r = Math.round(Math.random() * 256);
@@ -10,14 +8,13 @@ function randomBgColor(){
     return bgColor;
 }
 
-
 function grid(number){
-    container.style['grid-template-columns'] = 'repeat(' + number + ', 1fr)'; 
+    container.style['grid-template-columns'] = 'repeat(' + number + ', 1fr)';
     for (let x = 0; x < (number*number); x++){
         let square = document.createElement('div');
         square.classList.add('grid');
         container.append(square);
-    colorEtch();
+        square.addEventListener('mouseover', paint);
     };
 }
 
@@ -34,15 +31,35 @@ function colorEtch(){
         });
     }
 
-function empty(){
-    document.querySelectorAll('.grid').forEach((grid) => {
-        grid.style.background = 'beige';
-    });
-}
 
+const colours = document.querySelector('#rgb');
+colours.addEventListener('click', colorEtch);
 
 const clearGrid = document.querySelector('#clear');
-clearGrid.addEventListener('click', empty);
+clearGrid.addEventListener('click', ()=>{
+    document.querySelectorAll('.grid').forEach((grid)=>{
+        grid.style.background = 'beige';
+    });
+});
+
+const black = document.querySelector('#black');
+black.addEventListener('click', ()=>{
+    document.querySelectorAll('.grid').forEach((grid)=>{
+        grid.addEventListener('mouseenter', paint)
+    });
+});
+
+/*const greyScale = document.querySelector('#grey');
+greyScale.addEventListener('click', ()=>{
+    document.querySelectorAll('.grid').forEach((grid)=>{
+        grid.addEventListener('mouseover', ()=>{
+            if (grid.style.background === 'hsl(0, 0%' + 90 + '%)'){
+                grid.style.background = 'hsl(0, 0%' + 80 + '%)';
+            }
+        })
+    });
+})*/
+
 
 const resizeGrid = document.querySelector('#resize');
 resizeGrid.addEventListener('click', ()=>{
@@ -50,3 +67,10 @@ resizeGrid.addEventListener('click', ()=>{
     container.innerHTML = '';
     grid(input);
 });
+
+function paint(e){
+    e.target.style.background = 'black';
+}
+
+
+grid(10);
